afdec0e2bb36e93a7cf5c224ba4525651f82ccc3
MinhDuc30042002 <ducntm30042002@gmail.com>
2021-11-15 20:41:45 +0700

    chinh-sua-signup

--- signup.php
+++ signup.php
@@ -2,56 +2,61 @@
 
     echo "Sign Up Page";
 ?>
-<h1>Đăng kí thành viên</h1>
+<link rel="stylesheet" href="asset/css/style.css">
 <div class="form">
+    <h3>Đăng kí thành viên</h1>
     <form action="#" onsubmit="return KiemTra()" method="post" id="frmdangky">
-    <div  class="row mb10">
-        Tên đăng nhập<br>
-    <input type="text" id="name" name="name">
+    <div class="form-group">
+        <label for="">Tên đăng nhập</label> <br>
+        <input type="text" id="name" name="name">
     <div id="error1" class="error"></div>
     </div>
-    <div  class="row mb10">
-        Mật khẩu<br>
+    <div class="form-group">
+        <label for="">Mật khẩu</label> <br>
         <input type="password" id="pass" name="pass">
     <div id="error2" class="error"></div>
+
     </div>
-    <div  class="row mb10" >
-        Nhập lại mật khẩu<br>
+    <div class="form-group">
+        <label for="">Nhập lại mật khẩu</label> <br>
         <input type="password" id="repass" name="repass">
     <div id="error3" class="error"></div>
+
     </div>
-    <div  class="row mb10">
-        Ngày sinh<br>
-        <input type="date" id="date" name="date" >
+    <div class="form-group">
+        <label for="">Ngày sinh</label> <br>
+        <input type="date" id="date" name="date">
+
     </div>
-    <div  class="row mb10">
-        Email<br>
+    <div class="form-group">
+        <label for="">Email</label> <br>
         <input type="email" id="email" name="email">
-        <div id="error4" class="error"></div>
+    <div id="error4" class="error"></div>
+
     </div>
-    <div  class="row mb10">
-        Số điện thoại<br>
-        <input type="text" id="sdt" name="sdt">
-        <div id="error5" class="error"></div>
+    <div class="form-group">
+        <label for="">Số điện thoại</label> <br>
+        <input type="number" id="sdt" name="sdt">
+    <div id="error5" class="error"></div>
+
     </div>
-    <div  class="row mb10">
-        Hình ảnh(không bắt buộc)<br>
-        <input type="file" id="img" name="img">
+    <div class="form-group">
+        <label for="">Hình ảnh</label> <br>
+        <input type="file" id="file" name="file">
     </div>
-    <div  class="row mb10">
-        Tiều sử bệnh<br>
-        <div class="inp">
-            <input type="radio" name="tiensu"><span>Có</span>
-            <input type="radio" name="tiensu" checked=""><span>Không</span>
-        </div>
-        <div id="error6" class="error"></div>
+    <div class="form-group">
+        <label for="">Tiếu sử bệnh</label> <br>
+        <textarea name="text" id="text-area"></textarea>
+    <div id="error6" class="error"></div>
     </div>
-    <div  class="row mb10">
-        Mô tả bệnh<br>
-        <textarea name="text " id="note" cols="46" rows="5" maxlength="200"></textarea>
+    <div class="form-group">
+        <label for="">Mô tả bệnh</label> <br>
+        <textarea name="text" id="text-area"></textarea>
     <div id="error7" class="error"></div>
     </div>
-    <input type="submit" value="Đăng ký">
+    <div class="form-group">
+        <button id="btn-submit">Đăng ký</button>
+    </div>
     </form>
 </div>
 <script src="asset/resgist.js"></script>

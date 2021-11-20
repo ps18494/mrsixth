81d7a3c5767ccdcc307198c2ed6dee3cecd5fe98
ps18494 <tinntps18494backup2@gmail.com>
2021-11-20 15:50:46 +0700

    Tim kiem benh

--- controller/benh.php
+++ controller/benh.php
@@ -1,5 +1,8 @@
 <?php declare(strict_types=1);
 
+    require_once "dao/pdo.php";
+    require_once "dao/benh.php";
+
     function index() {
         return DEFAULT_VIEW . 'benh/index.php';
     }
@@ -9,5 +12,22 @@
     }
 
     function timkiem() {
+        global $query, $danhSachBenh;
+        $query = $_GET['q'] ?? NULL;
+        
+        if (!$query)
+        {
+            $danhSachBenh = [];
+        }
+        elseif (is_array($query))
+        {
+            $danhSachBenh = searchBenhByDanhSachTrieuChung($query);
+            $query = implode(",", $query); // Query để hiển thị ra view ngăn cách bởi dấu phẩy
+        }
+        else 
+        {
+            $danhSachBenh = searchBenhByTrieuChung($query);
+        }
+
         return DEFAULT_VIEW . 'benh/timkiem.php';
     }

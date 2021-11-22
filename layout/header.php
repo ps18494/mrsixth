8170a4e411ced3b09f32f7cf4689e359929caf95
MinhDuc30042002 <ducntm30042002@gmail.com>
2021-11-22 18:06:58 +0700

    done-index

--- layout/header.php
+++ layout/header.php
@@ -1,42 +1,38 @@
 <nav class="navbar navbar-expand-lg navbar-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">Mr.Sixth</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Trang chủ</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="#">Thuốc</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="#">Bệnh</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="#">Liên hệ</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Tin tức
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </li>
-            </ul>
-            <form class="d-flex">
-                <input class="form-control me-2" type="search" placeholder="Nhập từ khóa" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Tìm kiếm</button>
-            </form>
-            <form class="d-flex">
-                <button class="btn btn-outline-success login" type="submit">Đăng nhập</button>
-                <button class="btn btn-outline-success signup" type="submit">Đăng ký</button>
-            </form>
-        </div>
+    <a class="navbar-brand" href="#">Mr.Sixth</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Trang chủ</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">Thuốc</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">Bệnh</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">Liên hệ</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Tin tức</a>
+                <div class="dropdown-menu" aria-labelledby="dropdownId">
+                    <a class="dropdown-item" href="#">Action 1</a>
+                    <a class="dropdown-item" href="#">Action 2</a>
+                </div>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Nhập từ khóa">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Tìm kiếm</button>
+        </form>
+        <form class="form-inline my-2 my-lg-0">
+            <button class="btn-dang-nhap btn btn-outline-success my-2 my-sm-0" type="submit">Đăng ký</button>
+            <button class="btn-dang-ky btn btn-outline-success my-2 my-sm-0" type="submit">Đăng nhập</button>
+        </form>
     </div>
 </nav>
+<!-- end -->
--- view/forget_password.php
+++ view/forget_password.php
@@ -6,20 +6,24 @@ echo "Forget Password Page";
 ?>
 <link rel="stylesheet" href="asset/css/style.css">
 <div class="form-forget-password">
-    <form action="#" onsubmit="return KiemTra()" method="post" id="form-forget">
-        <img src="asset/icons/forgot.png" alt="img-forget">
-        <h3>Quên mật khẩu</h3>
-        <div class="form-group">
-            <label for="email">Nhập địa chỉ email</label> <br>
-            <input placeholder="Vui lòng nhập địa chỉ email" type="email" id="input-email" name="email">
-            <div id="error4" class="error"></div>
+    <form action="#" onsubmit="return KiemTra()" method="post" class="form-forget">
+        <div class="img-forget-password">
+            <img src="asset/icons/forgot.png" alt="img-forget">
         </div>
-        <div class="form-group">
-            <button id="btn-forget">Lấy lại mật khẩu</button>
+        <h3>Quên mật khẩu</h3>
+        <div class="enter-email">
+            <div class="form-input-forget form-group">
+                <label for="email">Nhập địa chỉ email</label> <br>
+                <input placeholder="Vui lòng nhập địa chỉ email" type="email" class="input-email" name="email">
+                <div id="error4" class="error"></div>
+            </div>
+            <div class="form-input-forget form-group">
+                <button class="btn-forget">Lấy lại mật khẩu</button>
+            </div>
         </div>
     </form>
 </div>
-<svg id="circle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 768" fill="none">
-    <circle cx="864.5" cy="351.5" r="864.5" fill="white" />
+<svg class="circle" viewBox="0 0 512 768" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="864.5" cy="351.5" r="864.5" fill="#91B6F9" />
 </svg>
 <script src="asset/resgist.js"></script>

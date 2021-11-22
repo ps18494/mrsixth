cbb4b058aaeb734ccf91c6859941c649db3af2a6
ps18494 <tinntps18494backup2@gmail.com>
2021-11-22 20:46:59 +0700

    Fix styles header, footer

--- layout/header.php
+++ layout/header.php
@@ -1,37 +1,42 @@
 <nav class="navbar navbar-expand-lg navbar-light">
-    <a class="navbar-brand" href="#">Mr.Sixth</a>
+    <a class="navbar-brand" href="<?= ROOT_DOMAIN ?>">Mr.Sixth</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Trang chủ</a>
+                <a class="nav-link active" aria-current="page" href="<?= ROOT_DOMAIN ?>">Trang chủ</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#">Thuốc</a>
+                <a class="nav-link" aria-current="page" href="<?=ROOT_DOMAIN . "/thuoc"?>">Thuốc</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#">Bệnh</a>
+                <a class="nav-link" aria-current="page" href="<?=ROOT_DOMAIN . "/benh"?>">Bệnh</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#">Liên hệ</a>
+                <a class="nav-link" aria-current="page" href="<?=ROOT_DOMAIN . "/lienhe"?>">Liên hệ</a>
             </li>
-            <li class="nav-item dropdown">
+            <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="<?=ROOT_DOMAIN . "/tintuc"?>">Tin tức</a>
+            </li>
+            <!-- <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Tin tức</a>
                 <div class="dropdown-menu" aria-labelledby="dropdownId">
                     <a class="dropdown-item" href="#">Action 1</a>
                     <a class="dropdown-item" href="#">Action 2</a>
                 </div>
-            </li>
+            </li> -->
         </ul>
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="text" placeholder="Nhập từ khóa">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Tìm kiếm</button>
+        <form class="header-search-form" action="<?= ROOT_DOMAIN . "/benh/timkiem"?>">
+            <div class="d-flex justify-between ">
+                <input name="q" class="form-control mr-sm-2" type="text" placeholder="Nhập từ khóa">
+                <button><i class="fa fa-search"></i></button>
+            </div>
         </form>
-        <form class="form-inline my-2 my-lg-0">
-            <button class="btn-dang-nhap btn btn-outline-success my-2 my-sm-0" type="submit">Đăng ký</button>
-            <button class="btn-dang-ky btn btn-outline-success my-2 my-sm-0" type="submit">Đăng nhập</button>
+        <form>
+            <button class="btn-dang-nhap btn btn-outline-success" type="submit">Đăng ký</button>
+            <button class="btn-dang-ky btn btn-outline-success" type="submit">Đăng nhập</button>
         </form>
     </div>
 </nav>

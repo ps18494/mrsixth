4e67fed1509f4112c67dc755474e88b6aa1fcc9e
ps18494 <tinntps18494backup2@gmail.com>
2021-12-02 23:29:23 +0700

    User view

---
+++ view/user/thongtin.php
@@ -0,0 +1,102 @@
+<?php extract($chitietuser); ?>
+<div class="container border-bottom bg-white mt-1 pt-md-3 pt-2 h">
+    <div class="d-flex flex-md-row justify-content-around align-items-center border-bottom">
+        <div>
+            <a href="<?= ROOT_DOMAIN . "/user/thongtin/"?>">
+                <svg data-toggle="tooltip" title="Chỉnh sửa" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                    <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                </svg>
+            </a>
+        </div>
+        <div class="d-flex flex-md-row align-items-center">
+            <div class="p-md-2"> 
+                <?php if ($hinh_anh) { ?> 
+                    <img style="width: 64px" src="<?= UPLOADS . "/img_user/$hinh_anh"?>" alt="" class="rounded-circle" id="profile"> 
+                <?php } else { ?>
+                    <img style="width: 64px" src="<?= UPLOADS . "/img_user/default.png"?>" alt="" class="rounded-circle" id="profile"> 
+                
+                <?php } ?>
+            </div>
+            <div class="p-md-2 p-1" id="info">
+                <h5><?=$id_user?> <?php if ($ten) { ?> <?="($ten)"?><?php } ?></h5>
+                <div class="text-muted">
+                    <?php if ($vai_tro == 1) { ?>
+                        Adminstrator
+                    <?php } else { ?>
+                        Client
+                    <?php } ?>
+                </div>
+            </div>
+        </div>
+        <div class="d-flex flex-column" id="info">
+            <div class="p-md-1 text-muted"> <span class="fa fa-envelope-o bg-light p-1 rounded-circle"></span> <?=$email?></div>
+            <div class="p-md-1 pt-sm-1 text-muted"> <span class="fa fa-phone bg-light p-1 rounded-circle"></span> <?=$sdt?></div>
+        </div>
+        <div class="rounded p-lg-2 p-1" id="blue-background">
+            <div class="d-flex flex-md-row align-items-center">
+                <div class="d-flex flex-column align-items-center px-lg-3 px-md-2 px-1" id="border-right">
+                    <p class="h4" data-toggle="tooltip" title="Số bệnh đã quan tâm"><?=$countBenh?></p>
+                    <div class="text-muted" id="count">Quan tâm</div>
+                </div>
+                <div class="d-flex flex-column align-items-center px-lg-3 px-md-2 px-1" id="border-right">
+                    <p class="h4 text-warning" data-toggle="tooltip" title="Đề xuất đang chờ quản trị viên xem xet"><?=$soDeXuatDoiDuyet?></p>
+                    <div class="text-muted" id="count">Đợi xem xét</div>
+                </div>
+                <div class="d-flex flex-column align-items-center px-lg-3 px-md-2 px-1" id="border-right">
+                    <p class="h4 text-success" data-toggle="tooltip" title="Số đề xuất đã được duyệt"><?=$soDeXuatDuocDuyet?></p>
+                    <div class="text-muted" id="count">Chấp nhận</div>
+                </div>
+                <div class="d-flex flex-column align-items-center px-lg-4 px-md-2 px-sm-1 px-2">
+                    <p class="h4 text-danger" data-toggle="tooltip" title="Số đề xuất đã bị từ chối"><?=$soDeXuatBiTuChoi?></p>
+                    <div class="text-muted" id="count">Từ chối</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="d-flex flex-md-row justify-content-around align-items-center my-4">
+        <form enctype="multipart/form-data" method="POST">
+            <div class="form-group row">
+                <label for="hinh_anh" class="col-sm-6 col-form-label">Ảnh đại diện</label>
+                <div class="col-sm-10">
+                    <input type="file" class="form-control" name="hinh_anh" id="hinh_anh" placeholder="Ảnh đại diện">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="ten" class="col-sm-6 col-form-label">Tên</label>
+                <div class="col-sm-10">
+                    <input value="<?=$ten?>" type="text" class="form-control" name="ten" id="ten" placeholder="Họ tên">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="ngay_sinh" class="col-sm-6 col-form-label">Ngày sinh</label>
+                <div class="col-sm-10">
+                    <input  value="<?=$ngay_sinh?>" type="date" class="form-control" name="ngay_sinh" id="ngay_sinh" placeholder="Ngày sinh">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="email" class="col-sm-6 col-form-label">Email</label>
+                <div class="col-sm-10">
+                    <input  value="<?=$email?>" type="text" class="form-control" name="email" id="email" placeholder="Email">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="sdt" class="col-sm-6 col-form-label">Số điện thoại</label>
+                <div class="col-sm-10">
+                    <input  value="<?=$sdt?>" type="text" class="form-control" name="sdt" id="sdt" placeholder="Số điện thoại">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="tinhtrangsuckhoe" class="col-sm-6 col-form-label">Tình trạng sức khỏe</label>
+                <div class="col-sm-10">
+                    <textarea type="file" class="form-control" name="tinhtrangsuckhoe" id="tinhtrangsuckhoe" placeholder="Tình trạng sức khỏe"><?=$tinh_trang_suc_khoe?></textarea>
+                </div>
+            </div>
+
+            <div >
+                <input type="hidden" name="id_user" value="<?=$id_user?>">
+                <input class="btn btn-primary" type="submit" name="capnhat" value="Cập nhật">
+            </div>
+        </form>
+    </div>
+</div>

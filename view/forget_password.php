99b27988e975c241728876e52cb8ca99f2562718
ps18494 <tinntps18494backup2@gmail.com>
2021-12-02 19:26:23 +0700

    Restore styling

--- view/forget_password.php
+++ view/forget_password.php
@@ -1,30 +1,30 @@
-<div >
+<div class="form-forget-password">
     <?php if (isset($MESSAGES)) { ?>
-        <div >
+        <div class="px-4">
             <?php foreach($MESSAGES as $msg) { ?>
-                <div  role="alert">
+                <div class="alert alert-warning" role="alert">
                     <?= $msg ?>
                 </div>
             <?php } // End loop $MESSAGE ?>
         </div>
     <?php } // End if?> 
-    <form method="post" >
-        <div >
+    <form method="post" class="form-forget">
+        <div class="img-forget-password">
             <img src="<?= ASSET . "icons/forgot.png"?>" alt="img-forget">
         </div>
         <h3>Quên mật khẩu</h3>
-        <div >
-            <div >
+        <div class="enter-email">
+            <div class="form-input-forget form-group">
                 <label for="email">Nhập địa chỉ email</label> <br>
-                <input placeholder="Vui lòng nhập địa chỉ email" type="email"  name="email" required>
+                <input placeholder="Vui lòng nhập địa chỉ email" type="email" class="input-email" name="email" required>
             </div>
-            <div >
-                <button type="submit" >Lấy lại mật khẩu</button>
+            <div class="form-input-forget form-group">
+                <button type="submit" class="btn-forget">Lấy lại mật khẩu</button>
             </div>
         </div>
     </form>
 </div>
-<svg  viewBox="0 0 512 768" fill="none" xmlns="http://www.w3.org/2000/svg">
+<svg class="circle" viewBox="0 0 512 768" fill="none" xmlns="http://www.w3.org/2000/svg">
     <circle cx="864.5" cy="351.5" r="864.5" fill="#91B6F9" />
 </svg>
 <script>
